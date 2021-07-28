124c5c5374a2fe4cafa064897d893745223b8994
JDVR25 <jd.villamilniandes.edu.co>
2021-07-28 17:04:23 -0500

    arreglo a el enrutamiento del navbar y gallery previous

---
+++ pages/gallery/previous/index.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="container">
+    <br>
+    <h2 class="text-center">
+      Sucesos previos a la constituyente
+    </h2>
+    <br>
+    <b-embed
+      type="iframe"
+      aspect="16by9"
+      src="https://www.youtube.com/embed/nb8tl_hPFS4"
+      allowfullscreen
+    />
+    <br>
+    <h4>
+      La Séptima Papeleta
+    </h4>
+    <p class="text_justify">
+      La Séptima Papeleta fue uno de los grandes impulsores de la Asamblea Nacional Constituyente y por medio de esta el pueblo invoco la creación de una nueva constitución.
+    </p>
+    <br>
+    <b-row>
+      <b-col cols="3">
+        <div>
+          <h5>Personalidades hablan sobre la séptima papeleta</h5>
+          <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/XO1IhESypN8"
+            allowfullscreen
+          />
+        </div>
+      </b-col>
+      <b-col cols="3">
+        <div>
+          <h5>¿Qué fue la séptima papeleta?</h5>
+          <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/qOzs-E71Wsk"
+            allowfullscreen
+          />
+        </div>
+      </b-col>
+      <b-col cols="3">
+        <div>
+          <h5>Claudia López promoviendo la séptima papeleta</h5>
+          <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/znEhgpdzglw"
+            allowfullscreen
+          />
+        </div>
+      </b-col>
+      <b-col cols="3">
+        <div>
+          <h5>Programa Contravía Séptima Papeleta</h5>
+          <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/pxpyxfQOi_Q"
+            allowfullscreen
+          />
+        </div>
+      </b-col>
+    </b-row>
+    <br>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+    >
+      <b-carousel-slide
+        v-for="(item, key) in imagesRoutes"
+        :key="key"
+        :img-src="item.link"
+      />
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      imagesRoutes: [
+        {
+          link: '/Mutimedia/Previo/papeleta/1.6.png'
+        },
+        {
+          link: '/Mutimedia/Previo/papeleta/1.7.png'
+        },
+        {
+          link: '/Mutimedia/Previo/papeleta/1.png'
+        },
+        {
+          link: '/Mutimedia/Previo/papeleta/2.jpeg'
+        },
+        {
+          link: '/Mutimedia/Previo/papeleta/3.jpeg'
+        },
+        {
+          link: '/Mutimedia/Previo/papeleta/4.png'
+        },
+        {
+          link: '/Mutimedia/Previo/papeleta/5.jpeg'
+        },
+        {
+          link: '/Mutimedia/Previo/papeleta/6.jpeg'
+        },
+        {
+          link: '/Mutimedia/Previo/papeleta/7.jpeg'
+        },
+        {
+          link: '/Mutimedia/Previo/papeleta/8.1.jpeg'
+        },
+        {
+          link: '/Mutimedia/Previo/papeleta/8.2.jpeg'
+        },
+        {
+          link: '/Mutimedia/Previo/papeleta/8.3.jpeg'
+        },
+        {
+          link: '/Mutimedia/Previo/papeleta/8.jpeg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
