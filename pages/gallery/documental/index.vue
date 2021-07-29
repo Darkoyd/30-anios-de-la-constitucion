eace88c2fe620633226fa70bda66b8f3add334ba
JDVR25 <jd.villamilniandes.edu.co>
2021-07-28 19:10:14 -0500

    Documental y foros

--- pages/gallery/documental/index.vue
+++ pages/gallery/documental/index.vue
@@ -1,18 +1,122 @@
 <template>
-  <div class="container">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fermentum
-    ligula eleifend, eleifend tortor in, egestas risus. Donec fermentum erat
-    nibh, eget lacinia metus vulputate ut. Integer at ullamcorper dui. Mauris eu
-    nibh vel est lobortis condimentum. Nullam euismod risus ligula, id dictum
-    libero molestie at. Integer auctor posuere feugiat. Donec ac magna tristique
-    arcu rutrum eleifend. Class aptent taciti sociosqu ad litora torquent per
-    conubia nostra, per inceptos himenaeos. Morbi id mollis erat, eu sodales
-    risus. Vestibulum ut ex fermentum, volutpat lacus non, vestibulum lorem.
-    Morbi sed tortor quis dolor convallis placerat. Integer velit ex, accumsan
-    et ultricies sed, aliquet eu turpis. Nullam bibendum nulla id nulla egestas
-    lacinia. Donec malesuada mi ante, eu tristique quam iaculis ut. Nullam ut
-    sapien dapibus, blandit metus at, aliquet massa. Quisque fermentum volutpat
-    justo a vulputate.
+  <div class="container py-5">
+    <h2 class="text-center">
+      Documentales y Foros
+    </h2>
+    <b-row class="py-3">
+      <b-col cols="4">
+        <div>
+          <h5>Encuentro: 20 años de la Constitución</h5>
+          <p>Uniandes – Economía</p>
+        </div>
+      </b-col>
+      <b-col cols="4">
+        <div>
+          <h5>30 años de la Constitución de 1991: Fidel Cano y Santiago Rivas conversan sobre la carta política</h5>
+        </div>
+      </b-col>
+      <b-col cols="4">
+        <div>
+          <h5>30 años de la ANC: ¿Cuál es el legado de la Asamblea Nacional Constituyente?</h5>
+        </div>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="4">
+        <div>
+          <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/puY20AzHt-I"
+            allowfullscreen
+          />
+        </div>
+      </b-col>
+      <b-col cols="4">
+        <div>
+          <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/jOKPBbAEckM"
+            allowfullscreen
+          />
+        </div>
+      </b-col>
+      <b-col cols="4">
+        <div>
+          <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/6C754jL2rJI"
+            allowfullscreen
+          />
+        </div>
+      </b-col>
+    </b-row>
+    <b-row class="py-3">
+      <b-col cols="3">
+        <div>
+          <h5>Foro Nacional "El Estado del Estado" Panel 2: La Constitución de 1991</h5>
+        </div>
+      </b-col>
+      <b-col cols="3">
+        <div>
+          <h5>Territorial Tolima: Celebración de los 30 años de la constitución política de 1991</h5>
+        </div>
+      </b-col>
+      <b-col cols="3">
+        <div>
+          <h5>SÉPTIMA PAPELETA: 30 AÑOS DEL MOVIMIENTO ESTUDIANTIL</h5>
+        </div>
+      </b-col>
+      <b-col cols="3">
+        <div>
+          <h5>Multiculturalismo vs. Universalismo en la Constitución de 1991 por Jesús Martín-Barbero.</h5>
+        </div>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="3">
+        <div>
+          <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/kLigd32ga_8"
+            allowfullscreen
+          />
+        </div>
+      </b-col>
+      <b-col cols="3">
+        <div>
+          <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/fKZyK8o3yng"
+            allowfullscreen
+          />
+        </div>
+      </b-col>
+      <b-col cols="3">
+        <div>
+          <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/Ob9ORDBaO10"
+            allowfullscreen
+          />
+        </div>
+      </b-col>
+      <b-col cols="3">
+        <div>
+          <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/hnUFv8MYqSE"
+            allowfullscreen
+          />
+        </div>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
