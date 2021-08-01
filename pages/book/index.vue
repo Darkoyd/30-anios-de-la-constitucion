c4e9a6f42aff801490774f95feb21fa9743a1e66
Catalina Londoño <nilocuru@hotmail.com>
2021-07-31 20:21:29 -0500

    Changed color scheme

--- pages/book/index.vue
+++ pages/book/index.vue
@@ -3,11 +3,11 @@
     <h1>Libro</h1>
     <br>
     <div v-for="(item, key) in libroParsed" :key="key">
-      <b-jumbotron :header="key+1 + ' - ' +item.title">
-        <hr class="my-4">
-        <b-button :href="item.link" variant="primary" target="_blank" class="jumboCardButtons" size="lg">
-          Ver más
+      <b-jumbotron :header="key+1 + ' - ' +item.title" class="jumboCard">
+        <b-button :href="item.link" variant="primary" target="_blank" class="jumboCardButtons">
+          Abrir cuadro
         </b-button>
+        <hr class="my-2">
       </b-jumbotron>
     </div>
   </div>
@@ -29,5 +29,11 @@ export default {
 <style>
 .jumboCardButtons {
   margin-left: 80%;
+}
+h1 {
+  font-size: 2em !important;
+}
+.jumboCard {
+  padding: 2em !important;
 }
 </style>
