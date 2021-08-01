b4fa77fccba628d2f495efce6571a40601f13c6b
JDVR25 <jd.villamilniandes.edu.co>
2021-08-01 17:48:09 -0500

    convocatoria asamblea

--- pages/materials/convocatoria/index.vue
+++ pages/materials/convocatoria/index.vue
@@ -1,23 +1,54 @@
 <template>
-  <div class="container">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fermentum
-    ligula eleifend, eleifend tortor in, egestas risus. Donec fermentum erat
-    nibh, eget lacinia metus vulputate ut. Integer at ullamcorper dui. Mauris eu
-    nibh vel est lobortis condimentum. Nullam euismod risus ligula, id dictum
-    libero molestie at. Integer auctor posuere feugiat. Donec ac magna tristique
-    arcu rutrum eleifend. Class aptent taciti sociosqu ad litora torquent per
-    conubia nostra, per inceptos himenaeos. Morbi id mollis erat, eu sodales
-    risus. Vestibulum ut ex fermentum, volutpat lacus non, vestibulum lorem.
-    Morbi sed tortor quis dolor convallis placerat. Integer velit ex, accumsan
-    et ultricies sed, aliquet eu turpis. Nullam bibendum nulla id nulla egestas
-    lacinia. Donec malesuada mi ante, eu tristique quam iaculis ut. Nullam ut
-    sapien dapibus, blandit metus at, aliquet massa. Quisque fermentum volutpat
-    justo a vulputate.
+  <div class="container py-5">
+    <b-container>
+      <h2 class="text-center">
+        Proceso legal que llevo a la constitución de 1991
+      </h2>
+      <b-jumbotron
+        v-for="(item, key) in procesos"
+        :key="key"
+        :header="item.titulo"
+      >
+        <b-container
+          class="but"
+        >
+          <p class>
+            {{ item.contenido }}
+          </p>
+        </b-container>
+        <b-button
+          variant="primary"
+          target="_blank"
+          :href="item.link"
+          class="jumboCardButtons"
+          size="lg"
+        >
+          Ver mas
+        </b-button>
+      </b-jumbotron>
+    </b-container>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      procesos: [
+        {
+          titulo: 'Decreto Legislativo 927 de 1990 “Por medio del cual se dictan medidas tendientes al restablecimiento del orden público”',
+          contenido: 'En este decreto el Gobierno de Virgilio Barco ordenó a la organización electoral contabilizar los votos que se habían depositado en las elecciones presidenciales de 1990.',
+          link: 'https://drive.google.com/file/d/1vMFP8C0ieQyPK_Nz--tsya7HoVy3BZMr/view?usp=sharing'
+        },
+        {
+          titulo: 'Sentencia del 9 de mayo de 1990 por la Corte Suprema de Justicia',
+          contenido: 'En esta sentencia el alto tribunal reconoce el poder constituyente del pueblo y avala la contabilización de los votos.',
+          link: 'https://drive.google.com/file/d/1cuKdUOr-JO52YGpG2mi5MLNGMAfrKwUd/view?usp=sharing'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style>
