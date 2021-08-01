56dddb13fc2f0c7319962ddf5ae79b1d26994ed3
JDVR25 <jd.villamilniandes.edu.co>
2021-08-01 01:34:30 -0500

    pagina de fallos relevantes

--- pages/materials/fallos/index.vue
+++ pages/materials/fallos/index.vue
@@ -1,24 +1,94 @@
 <template>
-  <div class="container">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fermentum
-    ligula eleifend, eleifend tortor in, egestas risus. Donec fermentum erat
-    nibh, eget lacinia metus vulputate ut. Integer at ullamcorper dui. Mauris eu
-    nibh vel est lobortis condimentum. Nullam euismod risus ligula, id dictum
-    libero molestie at. Integer auctor posuere feugiat. Donec ac magna tristique
-    arcu rutrum eleifend. Class aptent taciti sociosqu ad litora torquent per
-    conubia nostra, per inceptos himenaeos. Morbi id mollis erat, eu sodales
-    risus. Vestibulum ut ex fermentum, volutpat lacus non, vestibulum lorem.
-    Morbi sed tortor quis dolor convallis placerat. Integer velit ex, accumsan
-    et ultricies sed, aliquet eu turpis. Nullam bibendum nulla id nulla egestas
-    lacinia. Donec malesuada mi ante, eu tristique quam iaculis ut. Nullam ut
-    sapien dapibus, blandit metus at, aliquet massa. Quisque fermentum volutpat
-    justo a vulputate.
+  <div class="container py-5">
+    <b-container>
+      <h2 class="text-center">
+        Sentencias relevantes de la Corte Constitucional
+      </h2>
+      <b-jumbotron
+        v-for="(item, key) in sentencias"
+        :key="key"
+        :header="item.titulo"
+      >
+        <b-container
+          v-for="(elemento, llave) in item.numeros"
+          :key="llave"
+          class="but"
+        >
+          <p class="text-muted">
+            {{ elemento }}
+          </p>
+        </b-container>
+        <b-button
+          v-for="(enlace, lla) in item.links"
+          :key="lla"
+          variant="primary"
+          target="_blank"
+          :href="enlace"
+          class="jumboCardButtons"
+          size="lg"
+        >
+          {{ item.numeros[lla] }}
+        </b-button>
+      </b-jumbotron>
+    </b-container>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      sentencias: [
+        {
+          titulo: 'Despenalización parcial del aborto; interrupción voluntaria del embarazo como derecho reproductivo',
+          numeros: ['C-355 de 2006'],
+          miembros: ['M.P. Jaime Araujo Rentería', 'M.P. Clara Ines Vargas'],
+          links: ['https://drive.google.com/file/d/1KPWIRGHMnuJ-SYXakWeLR_9WjvyLQUGp/view?usp=sharing']
+        },
+        {
+          titulo: 'Derecho al agua como derecho social fundamental',
+          numeros: ['T-740 de 2011'],
+          miembros: ['M.P Humberto Sierra'],
+          links: ['https://drive.google.com/file/d/1mzZr0171u4Uxd3FamVTDZ63nz7s8dQ-i/view?usp=sharing']
+        },
+        {
+          titulo: 'Derecho a la consulta previa',
+          numeros: ['C-702 de 2010', 'C-608 de 2010'],
+          miembros: ['M.P Jorge Pretelt', 'M.P Humberto Sierra'],
+          links: ['https://drive.google.com/file/d/14eMlayq9wuJiV5o0Z_wapT2msgJTEqU6/view?usp=sharing', 'https://drive.google.com/file/d/1txw-C3fyHb0I_7AE2wvBydzVusQ49sdr/view?usp=sharing']
+        },
+        {
+          titulo: 'Derecho de las victimas del conflicto armado colombiano',
+          numeros: ['C-936 de 2010'],
+          miembros: ['M.P Luis Vargas'],
+          links: ['https://drive.google.com/file/d/1Acrm6tJ3scIKxb42RT0hhXlkslnzw3uk/view?usp=sharing']
+        },
+        {
+          titulo: 'Constitucionalidad de la Ley de cuotas (pdf pendiente)',
+          numeros: ['C-371/2000'],
+          miembros: ['M.P Carlos Gaviria Díaz'],
+          links: ['#']
+        },
+        {
+          titulo: 'Libre desarrollo de la personalidad en colegios (pdf pendiente)',
+          numeros: ['T-065/1993'],
+          miembros: ['M.P. Ciro Angarita Baron'],
+          links: ['#']
+        },
+        {
+          titulo: 'Estado de Cosas Iconstituconales (Sistema Carcelario)',
+          numeros: ['T-153/1998', 'T-388/2013'],
+          miembros: ['M.P. Eduardo Cifuente Muñoz', 'Maria Victoria Calle'],
+          links: ['https://drive.google.com/file/d/1amjkskkaDT30A9WiKvF1J7hlC2VL_Hry/view?usp=sharing', 'https://drive.google.com/file/d/1JSJBKXQpfrVuFEmj7Wu99WhQVRTH7kf5/view?usp=sharing']
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style>
+.but{
+  background-color: transparent;
+}
 </style>
